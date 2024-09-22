90db467a508070fe338b086ac0b82390fb50e5c6
Haebin <joker7011@naver.com>
2024-09-22 20:30:00 +0900

    feat: API Build Script

--- vacgom-api/build.gradle.kts
+++ vacgom-api/build.gradle.kts
@@ -10,3 +10,23 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
+
+tasks.register<Delete>("cleanResources") {
+    delete(file("src/main/resources"))
+}
+
+tasks.register<Copy>("initApiConfig") {
+    dependsOn("cleanResources")
+
+    from("API-CONFIG")
+    include("*.yml")
+
+    into("src/main/resources")
+    doFirst {
+        file("src/main/resources").mkdirs()
+    }
+}
+
+tasks.processResources {
+    dependsOn("initApiConfig")
+}
